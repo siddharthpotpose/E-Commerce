4d4a79ea355afa140fc3604c4b6ce428b4e56a6e
siddharthpotpose <siddharthpotpose9@gmail.com>
2023-04-06 17:27:05 +0530

    new update

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -9,6 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
 showData:any;
+productQuantity:number=1;
   constructor(private product:ServicesService,private active:ActivatedRoute){}
   ngOnInit(): void {
     let productData=this.active.snapshot.paramMap.get('ProductId')
@@ -16,5 +17,13 @@ showData:any;
     productData && this.product.getProductById(productData).subscribe((res)=>{
    this.showData=res;
     })
+  }
+  addProductMin(val:string){
+if(this.productQuantity<=9 && val === 'add'){
+this.productQuantity+=1
+}
+else if( this.productQuantity>1 && val === 'min'){
+ this.productQuantity-=1;
+}
   }
 }
